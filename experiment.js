f5b1a5af16b5cafa0d8c6f0dbfc9a64280a76d33
Flavio Duarte <flduarte13@gmail.com>
2017-09-24 18:53:01 -0300

    exeperiment changes

--- experiment.js
+++ experiment.js
@@ -21,8 +21,11 @@ var experiment = function (connection) {
     this.setExperimentAsFinished = function(result) {
 	console.log('update experimentos set vencedor = '+ result +' where id = ' + self.id + ';');
         connection.query('update experimentos set vencedor = ? where id = ?;', [result, self.id], function (err, results, fields) {
-            if (err) console.log('Erro finalizando experimento');
-            console.log(results);
+            if (err) {
+                console.log('Erro finalizando experimento');
+            } else {
+                console.log(results);
+            }
         });
     };
 };
